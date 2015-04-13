82d6f318f5cfde4a1fc0e6f9edf33f4690e84a82
Marembo Ochieng <marembo2008@gmail.com>
2015-04-13 20:59:56 +0200

    release

--- src/main/java/com/anosym/ip/mapping/IpMapping.java
+++ src/main/java/com/anosym/ip/mapping/IpMapping.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.anosym.ip.mapping;
 
 import com.anosym.utilities.Utility;
@@ -28,71 +24,77 @@ import org.htmlcleaner.HtmlCleaner;
  */
 public class IpMapping {
 
-  private static InputStream getIpMappingFile(int firstOctet, int secondOctet) {
-    return IpMapping.class.getResourceAsStream("/" + firstOctet + "/" + secondOctet + ".xml");
-  }
+    private static InputStream getIpMappingFile(int firstOctet, int secondOctet) {
+        return IpMapping.class.getResourceAsStream("/" + firstOctet + "/" + secondOctet + ".xml");
+    }
 
-  public static CountryIpMappings getCountryIpMappings(int firstOctet, int secondOctet) {
-    try {
-      InputStream ipStream = getIpMappingFile(firstOctet, secondOctet);
-      if (ipStream == null) {
+    public static CountryIpMappings getCountryIpMappings(int firstOctet, int secondOctet) {
+        try {
+            InputStream ipStream = getIpMappingFile(firstOctet, secondOctet);
+            if (ipStream == null) {
+                return null;
+            }
+            VDocument doc = VDocument.parseDocument(ipStream);
+            return new VMarshaller<CountryIpMappings>().unmarshall(doc);
+        } catch (VXMLBindingException ex) {
+            Logger.getLogger(IpMapping.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return null;
-      }
-      VDocument doc = VDocument.parseDocument(ipStream);
-      return new VMarshaller<CountryIpMappings>().unmarshall(doc);
-    } catch (VXMLBindingException ex) {
-      Logger.getLogger(IpMapping.class.getName()).log(Level.SEVERE, null, ex);
     }
-    return null;
-  }
 
-  public static CountryCode findCountryCodeFromIpAddress(String ipAddress) {
-    Ipv4 ip = new Ipv4(ipAddress);
-    return findCountryCodeFromIpAddress(ip);
-  }
+    public static CountryCode findCountryCodeFromIpAddress(String ipAddress) {
+        Ipv4 ip = new Ipv4(ipAddress);
+        return findCountryCodeFromIpAddress(ip);
+    }
 
-  private static CountryCode searchCountryCodeFromIp(Ipv4 ip, int firstOctet, int fromSecondOctet, int toSecondOctet) {
-    if (fromSecondOctet > toSecondOctet) {
-      return null;
+    private static CountryCode searchCountryCodeFromIp(Ipv4 ip, int firstOctet, int fromSecondOctet, int toSecondOctet) {
+        if (fromSecondOctet > toSecondOctet) {
+            return null;
+        }
+        CountryIpMappings cims = getCountryIpMappings(firstOctet, fromSecondOctet);
+        if (cims != null) {
+            CountryIpMapping cim = cims.findCountryIpMapping(ip);
+            if (cim != null) {
+                return cim.getCountryCode();
+            }
+        }
+        return searchCountryCodeFromIp(ip, firstOctet, ++fromSecondOctet, toSecondOctet);
     }
-    CountryIpMappings cims = getCountryIpMappings(firstOctet, fromSecondOctet);
-    if (cims != null) {
-      CountryIpMapping cim = cims.findCountryIpMapping(ip);
-      if (cim != null) {
-        return cim.getCountryCode();
-      }
-    }
-    return searchCountryCodeFromIp(ip, firstOctet, ++fromSecondOctet, toSecondOctet);
-  }
 
-  public static CountryCode findCountryCodeFromIpAddress(Ipv4 ip) {
-    int initialSecondOctet = 0;
-    int lastSecondOctet = 255;
-    int secondOctet = ip.getSecondOctet();
-    CountryCode countryCode = searchCountryCodeFromIp(ip, ip.getFirstOctet(), initialSecondOctet, secondOctet);
-    if (countryCode == null) {
-      return searchCountryCodeFromIp(ip, ip.getFirstOctet(), secondOctet + 1, lastSecondOctet);
-    } else {
-      return countryCode;
+    public static CountryCode findCountryCodeFromIpAddress(Ipv4 ip) {
+        int initialSecondOctet = 0;
+        int lastSecondOctet = 255;
+        int secondOctet = ip.getSecondOctet();
+        CountryCode countryCode = searchCountryCodeFromIp(ip, ip.getFirstOctet(), initialSecondOctet, secondOctet);
+        if (countryCode == null) {
+            return searchCountryCodeFromIp(ip, ip.getFirstOctet(), secondOctet + 1, lastSecondOctet);
+        } else {
+            return countryCode;
+        }
     }
-  }
 
-  public static void runIpMapping(Ipv4 ip) {
-    try {
-      String url = "http://freegeoip.net/xml/" + ip.toString();
-      HtmlCleaner htmlCleaner = new HtmlCleaner();
-      CleanerProperties cp = htmlCleaner.getProperties();
-      String xml = IOUtils.toString(new URI(url).toURL()
-              .openConnection().getInputStream());
-      //      new SimpleXmlSerializer(cp).writeToStream(node, System.out);
-      VDocument html = VDocument.parseDocumentFromString(xml);
-      String isoCode = html.getRootElement().findChild("CountryCode").toContent();
-      CountryCode cc = Utility.findCountryCodeFromCountryIsoCode(isoCode);
-      System.out.println(cc);
-    } catch (IOException ex) {
-      Logger.getLogger(IpMapping.class.getName()).log(Level.SEVERE, null, ex);
-    } catch (URISyntaxException ex) {
-      Logger.getLogger(IpMapping.class.getName()).log(Level.SEVERE, null, ex);
+    public static void runIpMapping(Ipv4 ip) {
+        try {
+            String url = "http://freegeoip.net/xml/" + ip.toString();
+            HtmlCleaner htmlCleaner = new HtmlCleaner();
+            CleanerProperties cp = htmlCleaner.getProperties();
+            String xml = IOUtils.toString(new URI(url).toURL()
+                    .openConnection().getInputStream());
+            //      new SimpleXmlSerializer(cp).writeToStream(node, System.out);
+            VDocument html = VDocument.parseDocumentFromString(xml);
+            String isoCode = html.getRootElement().findChild("CountryCode").toContent();
+            CountryCode cc = Utility.findCountryCodeFromCountryIsoCode(isoCode);
+            System.out.println(cc);
+        } catch (IOException ex) {
+            Logger.getLogger(IpMapping.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(IpMapping.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-  }
+
+    public static void main(String[] args) {
+        final String ipAddress = "208.38.3.13";
+        final Ipv4 ip = new Ipv4(ipAddress);
+        runIpMapping(ip);
+    }
 }
